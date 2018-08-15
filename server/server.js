ff5b96c65c0edbce5fd53778bdebf1cf8428796a
Yuvaraj-K <yuvikaruna@gmail.com>
2018-08-15 12:38:22 +0530

    Added GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,58 +1,58 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todos');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todos');
 var { User } = require('./models/user');
 
 
-var app =express();
+var app = express();
 
 app.use(bodyParser.json());
-app.post('/users',(req,res)=>{
-    // console.log(req.body);
-    var user = new User({
-        email:req.body.email
-    });
 
-    user.save().then((doc)=>{
-        res.send(doc);
-    },(e)=>{
-        res.status(400).send(e);
-    })
-});
-
-
-app.get('/users',(req,res)=>{
-    User.find().then((user)=>{
-        res.send({user})
-    },(e)=>{
-        res.status(400).send(e);
-    })
-});
-
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
     // console.log(req.body);
     var todo = new Todo({
-        text:req.body.text
+        text: req.body.text
     });
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
         res.status(400).send(e);
     })
 });
 
 
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
-        res.send({todos})
-    },(e)=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos })
+    }, (e) => {
         res.status(400).send(e);
     })
-})
+});
 
-app.listen(3000,()=>{
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send('Invalid ID');
+    } else {
+        Todo.findById({
+            _id: id
+        }).then((todo) => {
+            if (!todo) {
+               return res.status(404).send('ID not found');
+            }
+            res.send(todo)
+        }, (e) => {
+            res.status(400).send();
+        });
+    }
+});
+
+app.listen(3000, () => {
     console.log('listening to 3000');
 });
