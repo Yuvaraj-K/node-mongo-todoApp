b578714820a53470e3b77d86ead0958fe496c256
Yuvaraj-K <yuvikaruna@gmail.com>
2018-08-15 23:42:28 +0530

    PATCH todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var { ObjectID } = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todos');
@@ -8,7 +9,7 @@ var { User } = require('./models/user');
 
 
 var app = express();
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -40,19 +41,19 @@ app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        res.status(404).send('Invalid ID');
-    } else {
-        Todo.findById({
-            _id: id
-        }).then((todo) => {
-            if (!todo) {
-               return res.status(404).send('ID not found');
-            }
-            res.send(todo)
-        }, (e) => {
-            res.status(400).send();
-        });
+        return res.status(404).send('Invalid ID');
     }
+    Todo.findById({
+        _id: id
+    }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('ID not found');
+        }
+        res.send(todo)
+    }, (e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -60,18 +61,43 @@ app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        res.status(404).send('Invalid ID');
-    } else {
-        Todo.findByIdAndRemove(id).then((todo) => {
-            if (!todo) {
-               return res.status(404).send('ID not found');
-            }
-            res.send(todo)
-        }, (e) => {
-            res.status(400).send();
-        });
+        return res.status(404).send('Invalid ID');
     }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('ID not found');
+        }
+        res.send(todo)
+    }, (e) => {
+        res.status(400).send();
+    });
+
 });
+
+app.patch('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid ID');
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completedAt = null;
+        body.completed = false;        
+    }
+    Todo.findByIdAndUpdate(id, { $set: body }, { $new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('ID not found');
+        }
+        res.send({ todo })
+    }, (e) => {
+        res.status(400).send();
+    });
+})
 
 app.listen(port, () => {
     console.log(`Starting in port ${port}`);
